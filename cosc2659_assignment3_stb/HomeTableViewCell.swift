7e28e1e5806dfd804b1ff97eb0644fb25c253e90
s3298792 <s3298792@rmit.edu.vn>
2018-01-16 18:49:25 +0700

    Add list of Category in Home View

---
+++ cosc2659_assignment3_stb/HomeTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  cosc2659_assignment3_stb
+//
+//  Created by rmit on 1/16/18.
+//
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ cosc2659_assignment3_stb/HomeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  cosc2659_assignment3_stb
+//
+//  Created by rmit on 1/16/18.
+//
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var categories = [Category]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! HomeTableViewCell
+        let category = categories[indexPath.row]
+        cell.nameLabel.text = category.name
+        cell.amountLabel.text = String(category.amount) + " vnd"
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let category1 = Category(name: "Leisure", amount: 100000)
+        let category2 = Category(name: "Love", amount: 300000)
+        let category3 = Category(name: "Social", amount: 200000)
+        categories += [category1, category2, category3]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
