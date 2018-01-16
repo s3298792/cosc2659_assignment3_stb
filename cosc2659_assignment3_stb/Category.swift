dec93df6f8f5c152d6af0545d95a6b849e6abc0d
s3298792 <s3298792@rmit.edu.vn>
2018-01-16 16:45:47 +0700

    Add Category Class

---
+++ cosc2659_assignment3_stb/Category.swift
@@ -0,0 +1,20 @@
+//
+//  Category.swift
+//  cosc2659_assignment3_stb
+//
+//  Created by rmit on 1/16/18.
+//
+//
+
+import Foundation
+
+class Category {
+//    Properties
+    var name: String
+    var amount: Int
+    
+    init(name: String, amount: Int) {
+        self.name = name
+        self.amount = amount
+    }
+}
