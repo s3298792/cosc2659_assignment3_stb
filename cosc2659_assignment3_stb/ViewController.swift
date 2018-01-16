3f13bfa9ca5bea05775a2610d6bf7308aefa16f3
rmit <rmit@s00204044n-15.rmit.edu.vn>
2018-01-16 13:57:13 +0700

    Initial Commit

---
+++ cosc2659_assignment3_stb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cosc2659_assignment3_stb
+//
+//  Created by rmit on 1/16/18.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
